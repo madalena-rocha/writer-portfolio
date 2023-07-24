3460b2b7662d26ed6fc43cfaa02479e14e9c44ba
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-07-24 16:38:32 -0300

    css improvements

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,8 +8,8 @@ export function Footer() {
       <p>© 2023 Marta Machado Rocha</p>
 
       <div>
-        <a href="https://www.instagram.com/marta.machado.rocha/" target="_blank"><InstagramLogo size={32} /></a>
-        <a href="https://www.facebook.com/marta.machadorocha.5" target="_blank"><FacebookLogo size={32} /></a>
+        <a href="https://www.instagram.com/marta.machado.rocha/" target="_blank"><InstagramLogo size={24} /></a>
+        <a href="https://www.facebook.com/marta.machadorocha.5" target="_blank"><FacebookLogo size={24} /></a>
       </div>
     </Container>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -10,5 +10,25 @@ export const Container = styled.header`
   > div {
     display: flex;
     gap: 4.5rem;
+        
+    a::after {
+      content: '';
+      width: 0%;
+      height: 2px;
+      background-color: var(--secondary-color);
+
+      position: relative;
+      bottom: -2rem;
+      left: -0.5rem;
+
+      display: block;
+
+      transition: width 0.2s;
+    }
+
+    a:hover::after {
+      padding-inline: 0.8rem;
+      width: 100%;
+    }
   }
 `;
