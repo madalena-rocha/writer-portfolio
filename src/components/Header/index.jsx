7ffbde300e170ebac394142231d2f43547b78e34
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-08-06 16:07:11 -0300

    improve component Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,18 @@
-import { Container } from "./styles";
+import { Container, Navigation } from "./styles";
 
 export function Header() {
   return (
     <Container>
       <h1>Marta Machado Rocha</h1>
 
-      <div>
-        <a href="#home">Home</a>
-        <a href="#about">Sobre</a>
-        <a href="#books">Livros</a>
-        <a href="#tales">Contos</a>
-      </div>
+      <Navigation>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">Sobre</a></li>
+          <li><a href="#books">Livros</a></li>
+          <li><a href="#tales">Contos</a></li>
+        </ul>
+      </Navigation>
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,34 +1,48 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+  grid-area: header;
+
+  width: 100%;
+  
   display: flex;
   align-items: center;
   justify-content: space-between;
 
   padding: 2.8rem 12.3rem;
+  background-color: var(--primary-color);
 
-  > div {
+  position: fixed;
+  z-index: 9999;
+
+  > h1 {
+    color: var(--tertiary-color);
+  }
+`;
+
+export const Navigation = styled.nav`
+  > ul {
     display: flex;
-    gap: 4.5rem;
+    gap: 3.2rem;
+  }
         
-    a::after {
-      content: '';
-      width: 0%;
-      height: 2px;
-      background-color: var(--secondary-color);
+  a::after {
+    content: '';
+    width: 0%;
+    height: 2px;
+    background-color: var(--secondary-color);
 
-      position: relative;
-      bottom: -2rem;
-      left: -0.5rem;
+    position: relative;
+    bottom: -2rem;
+    left: -0.5rem;
 
-      display: block;
+    display: block;
 
-      transition: width 0.2s;
-    }
+    transition: width 0.2s;
+  }
 
-    a:hover::after {
-      padding-inline: 0.8rem;
-      width: 100%;
-    }
+  a:hover::after {
+    padding-inline: 0.8rem;
+    width: 100%;
   }
 `;
